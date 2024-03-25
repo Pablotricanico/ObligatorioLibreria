57950dbafac9383423d6dd1294e6db523e8b23e7
fkellner21 <kellner.francisco@gmail.com>
2024-03-25 13:20:22 -0300

    validaciones de usuario y vista index del admin

--- Libreria/Libreria/Entidades/Usuario.cs
+++ Libreria/Libreria/Entidades/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogicaDeNegocio.Excepciones.Usuario;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,25 +25,91 @@ namespace LogicaDeNegocio.Entidades
         {
             ValidarEmail();
             ValidarNombre();
+            ValidarApellido();
+            ValidarPassword();
         }
         public void ValidarEmail()
         {
-            if (!Email.Contains("@") || string.IsNullOrEmpty(Email))
+            if (string.IsNullOrEmpty(Email)|| !Email.Contains("@"))
             {
-                throw new Exception("Excepcion de email");
+                throw new EmailInvalidoException();
             }
         }
         public void ValidarNombre()
         {
             if (string.IsNullOrEmpty(Nombre))
             {
-                throw new Exception();
+                throw new NombreInvalidoException();
             }
             if (!(Char.IsLetter(Nombre[0])) || !(Char.IsLetter(Nombre[Nombre.Length - 1])))
             {
-                throw new Exception();
+                throw new NombreInvalidoException();
             }
-
+            if(Nombre.Length > 2)
+            {
+                for (int i = 1; i <= Nombre.Length-2; i++)
+                {
+                    if (!Char.IsLetter(Nombre[i]) && Nombre[i] != '-' && Nombre[i]!=' ' && Nombre[i] != '`')
+                    {
+                        throw new NombreInvalidoException();
+                    }
+                }
+            }
+        }
+        public void ValidarApellido()
+        {
+            if (string.IsNullOrEmpty(Apellido))
+            {
+                throw new ApellidoInvalidoException();
+            }
+            if (!(Char.IsLetter(Apellido[0])) || !(Char.IsLetter(Apellido[Apellido.Length - 1])))
+            {
+                throw new ApellidoInvalidoException();
+            }
+            if (Apellido.Length > 2)
+            {
+                for (int i = 1; i <= Apellido.Length - 2; i++)
+                {
+                    if (!Char.IsLetter(Apellido[i]) && Apellido[i] != '-' && Apellido[i] != ' ' && Apellido[i] != '`')
+                    {
+                        throw new ApellidoInvalidoException();
+                    }
+                }
+            }
+        }
+        public void ValidarPassword()
+        {
+            if(Password == null || Password.Length < 6)
+            {
+                throw new PasswordInvalidoException();
+            }
+            bool may = true;
+            bool min = true;
+            bool dig = true;
+            bool punt = true;
+            foreach(char c in Password)
+            {
+                if (char.IsDigit(c))
+                { 
+                    dig = false;
+                }
+                if (char.IsLower(c))
+                {
+                    min = false;
+                }
+                if (char.IsUpper(c))
+                {
+                    may = false;
+                }
+                if (c == '.' || c == ';' || c == ',' || c == '!')
+                {
+                    punt = false;
+                }
+            }
+            if (dig||min||may||punt)
+            {
+                throw new PasswordInvalidoException();
+            }
         }
     }
 }
--- Libreria/WebApp/Controllers/UsuarioController.cs
+++ Libreria/WebApp/Controllers/UsuarioController.cs
@@ -1,9 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore;
+using LogicaDeNegocio.Entidades;
+using LogicaDeNegocio.Excepciones;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers
 {
     public class UsuarioController : Controller
     {
+        //armo la lista para poder probar el verificar de los admin
+        public static List<Administrador> _administradores = new List<Administrador>();
+        public IActionResult AdminIndex(string mensaje)
+        {
+            ViewBag.Mensaje=mensaje;
+            return View(_administradores);
+        }
+        public IActionResult AdminCreate() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AdminCreate(Administrador administrador)
+        {
+            try
+            {
+                administrador.Validar();
+                _administradores.Add(administrador);
+            }
+            catch (DominioExcpetion ex)
+            {
+                ViewBag.mensaje = ex.Message;
+            }
+            catch (Exception)
+            {
+                ViewBag.mensaje = "Hubo un error, contactese con el administrador.";
+            }
+            return View(administrador);
+        }
         public IActionResult Index()
         {
             return View();
